ddc32dbaa68649aee8886f41b1d1f05b571f9f40
Daniel Du <67810876+duwenyu19@users.noreply.github.com>
2023-10-19 23:33:26 -0400

    deleted comment

--- App.tsx
+++ App.tsx
@@ -1,51 +1,20 @@
-// import React, {useState} from 'react'
-// import { NavigationContainer } from '@react-navigation/native'
-// import BottomTabNavigator from './src/navigators/BottomTabNavigator'
-// import { SafeAreaView } from 'react-native-safe-area-context'
-// import { StyleSheet } from 'react-native'
-// import UserContext from './src/utilities/UserContext'
-
-// const App: React.FC = () => {
-//   const [userCount, setUserCount] = useState(0)
-// const [followersCount, setFollowersCount] = useState(0)
-
-  
-//   return (
-//     <UserContext.Provider value={{ userCount, setUserCount, followersCount, setFollowersCount, loadUserData }}>
-//       <SafeAreaView style={styles.container}>
-//         <NavigationContainer>
-//           <BottomTabNavigator />
-//         </NavigationContainer>
-//       </SafeAreaView>
-//     </UserContext.Provider>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   }
-// })
-
-// export default App
-
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import BottomTabNavigator from './src/navigators/BottomTabNavigator';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
-import UserContext from './src/utilities/UserContext';
-import { usersDataFollowers } from './src/utilities/usersDataFollowers'; // 请替换为正确的路径
-import { usersDataFollowing } from './src/utilities/usersDataFollowing'; // 请替换为正确的路径
+import React, { useState, useEffect } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import BottomTabNavigator from './src/navigators/BottomTabNavigator'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet } from 'react-native'
+import UserContext from './src/utilities/UserContext'
+import { usersDataFollowers } from './src/utilities/usersDataFollowers'
+import { usersDataFollowing } from './src/utilities/usersDataFollowing'
 
 const App: React.FC = () => {
-  const [userCount, setUserCount] = useState(0);
-  const [followersCount, setFollowersCount] = useState(0);
+  const [userCount, setUserCount] = useState(0)
+  const [followersCount, setFollowersCount] = useState(0)
 
   useEffect(() => {
-    setUserCount(Object.keys(usersDataFollowing).length);
-    setFollowersCount(Object.keys(usersDataFollowers).length);
-  }, []);
+    setUserCount(Object.keys(usersDataFollowing).length)
+    setFollowersCount(Object.keys(usersDataFollowers).length)
+  }, [])
 
   return (
     <UserContext.Provider value={{ userCount, setUserCount, followersCount, setFollowersCount }}>
@@ -55,13 +24,13 @@ const App: React.FC = () => {
         </NavigationContainer>
       </SafeAreaView>
     </UserContext.Provider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   }
-});
+})
 
-export default App;
+export default App
